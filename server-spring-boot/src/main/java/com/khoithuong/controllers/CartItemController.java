4891497d9fed8870d39fa511a045f90fbcf8dd87
quangkhoi035 <71625187+quangkhoi035@users.noreply.github.com>
2021-01-10 11:14:24 +0700

    v2

--- server-spring-boot/src/main/java/com/khoithuong/controllers/CartItemController.java
+++ server-spring-boot/src/main/java/com/khoithuong/controllers/CartItemController.java
@@ -2,14 +2,16 @@ package com.khoithuong.controllers;
 
 import com.khoithuong.entity.CartItem;
 import com.khoithuong.entity.Category;
+import com.khoithuong.entity.Product;
+import com.khoithuong.exception.ResourceNotFoundException;
 import com.khoithuong.repository.CartItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -23,4 +25,33 @@ public class CartItemController {
         return cartItemRepository.findAll();
     }
 
+    @PostMapping("/cart-items")
+    public CartItem createCartItem(@RequestBody CartItem cartItem) {
+        return cartItemRepository.save(cartItem);
+    }
+
+    @PutMapping("/cart-items/{id}")
+    public ResponseEntity<CartItem> updateCartItem(@PathVariable(value = "id") Integer cartId,
+                                                   @RequestBody CartItem cartItemDetails) throws ResourceNotFoundException {
+        CartItem cartItem = cartItemRepository.findById(cartId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart item not found for this id :: " + cartId));
+
+        cartItem.setProduct(cartItemDetails.getProduct());
+        cartItem.setQuantity(cartItemDetails.getQuantity());
+
+        final CartItem updatedCartItem = cartItemRepository.save(cartItem);
+        return ResponseEntity.ok(updatedCartItem);
+    }
+
+    @DeleteMapping("/cart-items/{id}")
+    public Map<String, Boolean> deleteProduct(@PathVariable(value = "id") Integer cartId)
+            throws ResourceNotFoundException {
+        CartItem cartItem = cartItemRepository.findById(cartId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart item not found for this id :: " + cartId));
+
+        cartItemRepository.delete(cartItem);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
 }
--- server-spring-boot/src/main/java/com/khoithuong/controllers/OrderController.java
+++ server-spring-boot/src/main/java/com/khoithuong/controllers/OrderController.java
@@ -1,15 +1,15 @@
 package com.khoithuong.controllers;
 
 import com.khoithuong.entity.Order;
-import com.khoithuong.entity.Product;
+import com.khoithuong.exception.ResourceNotFoundException;
 import com.khoithuong.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -21,5 +21,35 @@ public class OrderController {
     @GetMapping("/orders")
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
+    }
+
+    @PostMapping("/orders")
+    public Order createCartItem(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @PutMapping("/orders/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable(value = "id") Integer orderId,
+                                                   @RequestBody Order orderDetails) throws ResourceNotFoundException {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found for this id :: " + orderId));
+
+        order.setListCartItem(orderDetails.getListCartItem());
+        order.setAmount(orderDetails.getAmount());
+
+        final Order updatedOrder = orderRepository.save(order);
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    @DeleteMapping("/orders/{id}")
+    public Map<String, Boolean> deleteProduct(@PathVariable(value = "id") Integer orderId)
+            throws ResourceNotFoundException {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found for this id :: " + orderId));
+
+        orderRepository.delete(order);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
